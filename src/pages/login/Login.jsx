3c95f665c1f1b2bbe409d847d44581bbf0219134
ozelmel <69895481+ozelmel@users.noreply.github.com>
2023-02-22 14:06:52 +0300

    login styles added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import LoginStyle, {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./Login.style";
+// import mealSvg from "../../assets/meal.svg";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg />
+        <Header>{"<Mlh/>"} Recipe</Header>
+        <StyledForm>
+          <StyledInput type="text" placeholder="username" required />
+          <StyledInput type="password" placeholder="password" required />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
 
-export default Login
+export default Login;
